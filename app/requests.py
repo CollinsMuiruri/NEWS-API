760f3fbe48aef8651434d99f4027c92019961bd8
Collins Muiruri <wanyekicollins@gmail.com>
2018-04-10 12:49:06 +0300

    Add the articles

--- app/requests.py
+++ app/requests.py
@@ -1,5 +1,6 @@
+
 import urllib.request,json
-from .models import Source
+from .models import Source, Article
 
 api_key = None
 
@@ -25,7 +26,7 @@ def get_sources():
 
     return sources_results
 
-    def process_sources():
+def process_sources():
     '''
     Function to process the source results to change from JSON language
     '''
@@ -41,3 +42,35 @@ def get_sources():
         source_results.append(source_object)
 
     return source_results
+
+def get_articles():
+    '''
+    Function to get a source and it's get_articles
+    '''
+    get_articles_url = 'https://newsapi.org/v1/articles?source={}&apikey={}'.format(id,api_key)
+
+    with urllib.request.urlopen(get_articles_url) as url:
+        get_articles_data = url.read()
+        get_articles_response = json.loads(get_articles_data)
+
+        articles_results = None
+
+        if get_sources_response['articles']:
+            articles_results_list = get_articles_response['articles']
+            articles_results = process_articles(articles_results_list)
+
+    return articles_results
+
+def process_articles(article_list):
+    '''
+    Function that processes article results
+    '''
+    article_results = []
+
+    for article_item in article_list:
+        source = article_item.get('source')
+        title = article_item.get('title')
+        urlToImage = article_item.get('urlToImage')
+        description = article_item.get('description')
+        urlToArticle = article_item.get('url')
+        publishedAt = article_item.get('publishedAt')
