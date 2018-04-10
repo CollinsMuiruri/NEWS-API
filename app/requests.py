06b1a58016844b5c88f18f917cd512fd562b2df3
Collins Muiruri <wanyekicollins@gmail.com>
2018-04-10 11:10:26 +0300

    Add the sources

---
+++ app/requests.py
@@ -0,0 +1,26 @@
+import urllib.request,json
+from .models import Source, Article
+
+api_key = None
+
+def configure_request(app):
+    global api_key
+    api_key = app.connfig['NEWS_API_KEY']
+
+def get_sources():
+    '''
+    Getting json response o url requests
+    '''
+    get_sources_url = 'https://newsapi.org/v1/sources'
+
+    with urllib.request.urlopen(get_sources_url) as url:
+        get_sources_data = url.read()
+        geT_sources_response = json.loads(geT_sources_data)
+
+        sources_results = None
+
+        if get_sources_response['sources']:
+            sources_results_list = geT_sources_response['sources']
+            sources_results = process_sources(sources_results_list)
+
+    return sources_results
