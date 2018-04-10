5e8b36acec5bb9c3e51459917b080d01541d7f28
Collins Muiruri <wanyekicollins@gmail.com>
2018-04-10 12:28:45 +0300

    Add article index

--- app/main/views.py
+++ app/main/views.py
@@ -1,3 +1,4 @@
+
 from flask import render_template,request,redirect,url_for
 from . import main
 from ..requests import get_sources,get_articles
@@ -10,3 +11,12 @@ def index():
     title = "FlashNews - HomePage"
     sources = get_sources()
     return render_template('index.html', title=title, sources=sources)
+
+@main.route('/source/<source_id>')
+def source(source_id):
+    '''
+    View source page function that returns a source page and its data
+    '''
+    title = f"{source_id} page"
+    articles = get_articles(source_id)
+    return render_template('source.html', title=title, articles=articles)
